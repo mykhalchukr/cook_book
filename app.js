5d4429868eb4d1228db1bf0faeb861883a6b9905
Roma <roman.mykhalchuk@yahoo.com>
2020-07-15 00:52:22 +0300

    before depl

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const config = require("config");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -9,6 +10,14 @@ const PORT = config.get("port") || 5000;
 
 app.use(bodyParser.json());
 app.use("/api/recipes", require("./routes/api/recipes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 async function start() {
   try {
